ebb1b48979707f293eccc527a97c9b5871232677
mikolaj <mikolaj@statystyka.net>
2022-03-31 21:16:01 +0200

    Little changes in hunter

--- src/game/server/player_classes.h
+++ src/game/server/player_classes.h
@@ -10,7 +10,7 @@ enum class Class {
     Tank, //note:Offensive TODO Implement: make him heavy and slower, can hold players inf. make him stronk dmg/2 or dmg/1.5, spawn with grenade launcher and reload gun (machine gun) faster
     Spider, //note:Offensive/Defensive TODO make like in engineer walls (make that when two laser walls intersect one of them will stop on another (NO: --|--, but YES:   |--))
     Engineer, //note:Defensive TODO make that when two laser walls intersect one of them will stop on another (NO: --|--, but YES:   |--)
-    Armorer,//note:Defensive TODO make better name, implement: can (like medic place custom pickupables same 2 options, but for weapons), maybe can create aura of battle or slt. or give him ability to make turrets (Make 3 (while standing in place) hammer hits to create a turret)
+    Armorer,//note:Defensive TODO make better name, implement: can (like medic place custom pickupables same 2 options, but for weapons), maybe can create aura of battle or slt
     Necromancer//note:Offensive/Defensive|HARD to implement! TODO can summon offensive zombies and defensive ones, he is weak, after killing anyone he automatically summons free zombie private guard ! HARD TO IMPLEMENT !
     // TODO !MAYBE! add another class
 };
